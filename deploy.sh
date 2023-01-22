84d93ebab0f08950430a22b2be3e94376d5c0c02
Jindřich Pergler <jindrich@pergler.eu>
2023-01-22 14:31:21 +0100

    Add deploy script to separate branch

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run clean-build-public
+npm run build-public
+
+# navigate into the build output directory
+cd build/public
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f https://github.com/jpergler/zacilenonazdravi.git master:publish/pages
+
+cd -
